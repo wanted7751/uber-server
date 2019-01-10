6bc73957dd34156c6d2fcd897f734853f224cb62
parkminho <minho4377@daum.net>
2019-01-10 20:12:29 +0900

    #1.11 Creating a Virtual Environment on NodeJS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
 import {Options} from "graphql-yoga";
 import {createConnection} from "typeorm";
 import app from "./app";
 import connectionOptions from "./ormConfig";
 
+dotenv.config();
 
+console.log(process.env);
 
 const PORT: number | string = process.env.PORT || 4000;
 const PLAYGROUND_ENDPOINT  : string = "/playground";
@@ -21,7 +24,7 @@ const handleAppStart = () => console.log(`Listening on port ${PORT}`)
 
     createConnection(connectionOptions).then(() =>{
         app.start(appOptions, handleAppStart)
-    })
+    }).catch(error => console.log(error));
 
     
 
--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -6,12 +6,12 @@ const connectionOptions: ConnectionOptions = {
   synchronize: true,
   logging: true,
   entities: ["entities/**/*.*"],
-  host: process.env.DB_ENDPOINT || "localhost",
+  host: process.env.DB_ENDPOINT,
   //실제서버를 운영하면 DB_ENDPONIT AWS의 주소같은게 들어가야한다.
   port: 5432,
   // postgres의 포트는 항상같다. 5432
-    username: process.env.DB_USENAME || "",
-    password: process.env.DB_PASSWORD || ""
+    username: process.env.DB_USENAME, 
+    password: process.env.DB_PASSWORD 
 };
 
 export default connectionOptions;   
