ec6645498f239ca81c78a535143990a550f6c2bb
parkminho <minho4377@daum.net>
2019-01-10 15:41:45 +0900

    #1.5 GraphQL Yoga and Express part Two (여기까지 graphql과 함께 동작하는 express서버를 만든것!)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+import {Options} from "graphql-yoga";
 import app from "./app";
 
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT  : string = "/playground";
+const GRAPHQL_ENDPOINT : string = "/graphql";
+
+const appOptions: Options = {
+    port: PORT,
+    playground:PLAYGROUND_ENDPOINT,
+    endpoint:GRAPHQL_ENDPOINT
+
+};
+
+const handleAppStart = () => console.log(`Listening on port ${PORT}`)
+    // start의 콜백함수를 만드는 중.
+app.start(appOptions,handleAppStart)
 
 
+// 
 
-app.start()
